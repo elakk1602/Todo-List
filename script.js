1d0fd2a8a0c7e6555680bdda04f26c226afd992c
Elakkiya G <elakkiyae2005@gmail.com>
2025-06-08 13:22:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+  
+    if (taskText === "") return;
+  
+    const li = document.createElement("li");
+    li.textContent = taskText;
+  
+    li.addEventListener("click", () => {
+      li.classList.toggle("completed");
+    });
+  
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "✖";
+    deleteBtn.className = "delete-btn";
+    deleteBtn.onclick = () => li.remove();
+  
+    li.appendChild(deleteBtn);
+  
+    document.getElementById("taskList").appendChild(li);
+    taskInput.value = "";
+  }
+  
